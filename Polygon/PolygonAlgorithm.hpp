9a9107bc79b793c05fc00a85d72591a56ac6cb06
LingjieJin <908422490@163.com>
2020-05-11 11:25:40 +0800

    更新多边形
    Signed-off-by: LingjieJin <908422490@163.com>

--- Polygon/PolygonAlgorithm.hpp
+++ Polygon/PolygonAlgorithm.hpp
@@ -9,16 +9,15 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
+template <class T>
 class IPolygonAlgorithmBase
 {
-public:
-    IPolygonAlgorithmBase() {}
-    virtual ~IPolygonAlgorithmBase() {}
 
+    ~virtual IPolygonAlgorithmBase(){}
     /**
      * 判断点是否在线段上
      */
-    virtual bool isPointOnLine(double p0_x, double p0_y, double p1_x, double p1_y, double p2_x, double p2_y)
+    virtual bool isPointOnLine(T p0_x, T p0_y, T p1_x, T p1_y, T p2_x, T p2_y)
     {
         double d1 = (p1_x - p0_x) * (p2_y - p0_y) - (p2_x - p0_x) * (p1_y - p0_y);
         if ((abs(d1) < 0.000001) && ((p0_x - p1_x) * (p0_x - p2_x) <= 0) && ((p0_y - p1_y) * (p0_y - p2_y) <= 0))
@@ -28,18 +27,12 @@ public:
         return false;
     }
 
-    /**
-     * 判断点是否在线段上
-     */
-    virtual bool isPointOnLine(double p0_x, double p0_y, CLine &line)
+    virtual bool isPointOnLine(T p0_x, T p0_y, CLine<T> &line)
     {
         return isPointOnLine(p0_x, p0_y, line._p1._x, line._p1._y, line._p2._x, line._p2._y);
     }
 
-    /**
-     * 判断点是否在线段上
-     */
-    virtual bool isPointOnLine(CPoint &point, CLine &line)
+    virtual bool isPointOnLine(CPoint<T> &point, CLine<T> &line)
     {
         return isPointOnLine(point._x, point._y, line._p1._x, line._p1._y, line._p2._x, line._p2._y);
     }
@@ -48,24 +41,29 @@ public:
      * 判断点是否在矩形内
      * 包括矩形边上
      */
-    virtual bool isPointInRect(double x, double y, CRect &rect)
+    virtual bool isPointInRect(T x, T y, CRect<T> &rect)
     {
         if (x >= rect.p_min._x && x <= rect.p_max._x && y >= rect.p_min._y && y <= rect.p_max._y)
             return true;
         return false;
     }
 
+    virtual bool isPointInRect(CPoint<T> &point, CRect<T> &rect)
+    {
+        return isPointInRect(point._x, point._y, rect);
+    }
+
     /**
      * 判断点是否在多边形内
      * 包括多边形边上
      */
-    virtual bool isPointInPolygon(double x, double y, const CPolygon &p)
+    virtual bool isPointInPolygon(const T &x, const T &y, const CPolygon<T> &polygon)
     {
         bool isInside = false;
         int count = 0;
 
         // quickly check
-        CRect r = getPolygonEnvelopRect(p);
+        CRect<T> r = getPolygonEnvelopRect(p);
         if (!isPointInRect(x, y, r))
         {
             return false;
@@ -73,9 +71,9 @@ public:
 
         //
         double minX = 1.7976931348623158e+308; // double max value
-        for (int j = 0; j < p._vecPoints.size(); j++)
+        for (int j = 0; j < polygon._vecPoints.size(); j++)
         {
-            minX = MIN(minX, p._vecPoints[j]._x);
+            minX = MIN(minX, polygon._vecPoints[j]._x);
         }
 
         // judge point
@@ -89,12 +87,12 @@ public:
         double linePoint2y = y;
 
         //遍历每一条边
-        for (int i = 0; i < p._vecPoints.size(); i++)
+        for (int i = 0; i < polygon._vecPoints.size(); i++)
         {
-            double cx1 = p._vecPoints[i]._x;
-            double cy1 = p._vecPoints[i]._y;
-            double cx2 = p._vecPoints[(i + 1) % p._vecPoints.size()]._x;
-            double cy2 = p._vecPoints[(i + 1) % p._vecPoints.size()]._y;
+            double cx1 = polygon._vecPoints[i]._x;
+            double cy1 = polygon._vecPoints[i]._y;
+            double cx2 = polygon._vecPoints[(i + 1) % polygon._vecPoints.size()]._x;
+            double cy2 = polygon._vecPoints[(i + 1) % polygon._vecPoints.size()]._y;
 
             if (isPointOnLine(px, py, cx1, cy1, cx2, cy2))
             {
@@ -134,11 +132,19 @@ public:
         return isInside;
     }
 
+    virtual bool isPointInPolygon(const CPoint<T> &point, const CPolygon<T> &polygon)
+    {
+        return isPointInPolygon(point._x, point._y, polygon);
+    }
+
     /**
      * 判断线段是否相交
      * 如果线段交于一点 也是相交
      */
-    virtual bool isLineIntersect(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+    virtual bool isLineIntersect(T x1, T y1,
+                                 T x2, T y2,
+                                 T x3, T y3,
+                                 T x4, T y4)
     {
         /**
          * 快速排斥：
@@ -168,11 +174,10 @@ public:
         return (u * v <= 0.00000001 && w * z <= 0.00000001);
     }
 
-    /**
-     * 判断线段是否相交
-     * 如果线段交于一点 也是相交
-     */
-    virtual bool isLineIntersect(const CPoint &p1, const CPoint &p2, const CPoint &p3, const CPoint &p4)
+    virtual bool isLineIntersect(const CPoint<T> &p1,
+                                 const CPoint<T> &p2,
+                                 const CPoint<T> &p3,
+                                 const CPoint<T> &p4)
     {
         return isLineIntersect(p1._x, p1._y,
                                p2._x, p2._y,
@@ -180,47 +185,45 @@ public:
                                p4._x, p4._y);
     }
 
-    /**
-     * 判断线段是否相交
-     * 如果线段交于一点 也是相交
-     */
-    virtual bool isLineIntersect(const CLine &line_1, const CLine &line_2)
+    virtual bool isLineIntersect(const CLine<T> &l1, const CLine<T> &l2)
     {
-        return isLineIntersect(line_1._p1._x, line_1._p1._y,
-                               line_1._p2._x, line_1._p2._y,
-                               line_2._p1._x, line_2._p1._y,
-                               line_2._p2._x, line_2._p2._y);
+        return isLineIntersect(l1._p1, l1._p2, l2._p1, l2._p2);
     }
 
     /**
      * 求线段P1P2与Q1Q2的交点。
-     * 先进行快速排斥实验和跨立实验确定有交点再进行计算。
-     * 交点（x,y）使用引用返回。
      */
-    virtual bool GetCrossPoint(const CPoint &p1, const CPoint &p2, const CPoint &q1, const CPoint &q2, double &x, double &y)
+    virtual void GetCrossPoint(const CPoint<T> &p1,
+                               const CPoint<T> &p2,
+                               const CPoint<T> &q1,
+                               const CPoint<T> &q2,
+                               const T &x, const T &y)
     {
-        if (isLineIntersect(p1, p2, q1, q2))
-        {
-            //求交点
-            double tmpLeft, tmpRight;
-            tmpLeft = (q2._x - q1._x) * (p1._y - p2._y) - (p2._x - p1._x) * (q1._y - q2._y);
-            tmpRight = (p1._y - q1._y) * (p2._x - p1._x) * (q2._x - q1._x) + q1._x * (q2._y - q1._y) * (p2._x - p1._x) - p1._x * (p2._y - p1._y) * (q2._x - q1._x);
+        //求交点
+        double tmpLeft = 0.0, tmpRight = 0.0;
+        tmpLeft = (q2._x - q1._x) * (p1._y - p2._y) - (p2._x - p1._x) * (q1._y - q2._y);
+        tmpRight = (p1._y - q1._y) * (p2._x - p1._x) * (q2._x - q1._x) + q1._x * (q2._y - q1._y) * (p2._x - p1._x) - p1._x * (p2._y - p1._y) * (q2._x - q1._x);
 
-            x = ((double)tmpRight / (double)tmpLeft);
+        x = (tmpRight / tmpLeft);
 
-            tmpLeft = (p1._x - p2._x) * (q2._y - q1._y) - (p2._y - p1._y) * (q1._x - q2._x);
-            tmpRight = p2._y * (p1._x - p2._x) * (q2._y - q1._y) + (q2._x - p2._x) * (q2._y - q1._y) * (p1._y - p2._y) - q2._y * (q1._x - q2._x) * (p2._y - p1._y);
+        tmpLeft = (p1._x - p2._x) * (q2._y - q1._y) - (p2._y - p1._y) * (q1._x - q2._x);
+        tmpRight = p2._y * (p1._x - p2._x) * (q2._y - q1._y) + (q2._x - p2._x) * (q2._y - q1._y) * (p1._y - p2._y) - q2._y * (q1._x - q2._x) * (p2._y - p1._y);
 
-            y = ((double)tmpRight / (double)tmpLeft);
-            return true;
-        }
-        return false;
+        y = (tmpRight / tmpLeft);
     }
+
+    virtual void GetCrossPoint(const CLine<T> &l1,
+                               const CLine<T> &l2,
+                               const CPoint<T> &point)
+    {
+        return GetCrossPoint(l1._p1, l1._p2, l2._p1, l2._p2, point._x, point._y);
+    }
+
 
     /**
      * 判断多边形是否相交
      */
-    virtual bool isPolygonsIntersect(const CPolygon &p1, const CPolygon &p2)
+    virtual bool isPolygonsIntersect(const CPolygon<T> &p1, const CPolygon<T> &p2)
     {
         int i, j;
         // 1. point judge
@@ -254,12 +257,12 @@ public:
     /**
      * 获取多边形的最大正矩形
      */
-    virtual CRect getPolygonEnvelopRect(const CPolygon &p)
+    virtual CRect<T> getPolygonEnvelopRect(const CPolygon<T> &p)
     {
-        double minx = p._vecPoints[0]._x;
-        double maxx = p._vecPoints[0]._x;
-        double miny = p._vecPoints[0]._y;
-        double maxy = p._vecPoints[0]._y;
+        T minx = p._vecPoints[0]._x;
+        T maxx = p._vecPoints[0]._x;
+        T miny = p._vecPoints[0]._y;
+        T maxy = p._vecPoints[0]._y;
 
         for (int i = 1; i < p._vecPoints.size(); i++)
         {
@@ -272,297 +275,11 @@ public:
             if (p._vecPoints[i]._y > maxy)
                 maxy = p._vecPoints[i]._y;
         }
-        return CRect(minx, miny, maxx, maxy);
+        return CRect<T>(minx, miny, maxx, maxy);
     }
 
-    virtual bool getCrossPoint(const CPoint &p1, const CPoint &p2, const CPoint &q1, const CPoint &q2, long &x, long &y)
-    {
-        /**
-        求线段P1P2与Q1Q2的交点。
-        先进行快速排斥实验和跨立实验确定有交点再进行计算。
-        交点（x,y）使用引用返回。
-        没有验证过
-        **/
-        if (isLineIntersect(p1, p2, q1, q2))
-        {
-            //求交点
-            long tmpLeft, tmpRight;
-            tmpLeft = (q2._x - q1._x) * (p1._y - p2._y) 
-                        - (p2._x - p1._x) * (q1._y - q2._y);
-            tmpRight = (p1._y - q1._y) * (p2._x - p1._x) * (q2._x - q1._x) 
-                    + q1._x * (q2._y - q1._y) * (p2._x - p1._x) 
-                    - p1._x * (p2._y - p1._y) * (q2._x - q1._x);
 
-            x = (int)((double)tmpRight / (double)tmpLeft);
 
-            tmpLeft = (p1._x - p2._x) * (q2._y - q1._y) 
-                    - (p2._y - p1._y) * (q1._x - q2._x);
-            tmpRight = p2._y * (p1._x - p2._x) * (q2._y - q1._y) 
-                    + (q2._x - p2._x) * (q2._y - q1._y) * (p1._y - p2._y) 
-                    - q2._y * (q1._x - q2._x) * (p2._y - p1._y);
-            y = (int)((double)tmpRight / (double)tmpLeft);
-
-            return true;
-        }
-        return false;
-    }
-};
-
-class ISpPolygonAlgorithm : public IPolygonAlgorithmBase
-{
-public:
-    ISpPolygonAlgorithm() {}
-    ~ISpPolygonAlgorithm() {}
-
-    // 针对不考虑点在边缘的情况下做判断
-
-    /**
-     * 判断点是否在矩形内
-     * 不包括矩形边上
-     */
-    bool isPointInRect(double x, double y, CRect &rect)
-    {
-        if (x > rect.p_min._x && x < rect.p_max._x && y > rect.p_min._y && y < rect.p_max._y)
-            return true;
-        return false;
-    }
-
-    /**
-     * 判断点是否在多边形内
-     * 不包括多边形边上
-     */
-    bool isPointInPolygon(double x, double y, const CPolygon &p)
-    {
-        bool isInside = false;
-        int count = 0;
-
-        // quickly check
-        CRect r = getPolygonEnvelopRect(p);
-        if (!isPointInRect(x, y, r))
-        {
-            return false;
-        }
-
-        //
-        double minX = 1.7976931348623158e+308; // double max value
-        for (int j = 0; j < p._vecPoints.size(); j++)
-        {
-            minX = MIN(minX, p._vecPoints[j]._x);
-        }
-
-        // judge point
-        double px = x;
-        double py = y;
-        // line start point
-        double linePoint1x = x;
-        double linePoint1y = y;
-        // line end point
-        double linePoint2x = minX - 10; //取最小的X值还小的值作为射线的终点
-        double linePoint2y = y;
-
-        //遍历每一条边
-        for (int i = 0; i < p._vecPoints.size(); i++)
-        {
-            double cx1 = p._vecPoints[i]._x;
-            double cy1 = p._vecPoints[i]._y;
-            double cx2 = p._vecPoints[(i + 1) % p._vecPoints.size()]._x;
-            double cy2 = p._vecPoints[(i + 1) % p._vecPoints.size()]._y;
-
-            if (isPointOnLine(px, py, cx1, cy1, cx2, cy2))
-            {
-                return false;
-            }
-
-            if (fabs(cy2 - cy1) < 0.000001) //平行则不相交
-            {
-                continue;
-            }
-
-            if (isPointOnLine(cx1, cy1, linePoint1x, linePoint1y, linePoint2x, linePoint2y))
-            {
-                if (cy1 > cy2) //只保证上端点+1
-                {
-                    count++;
-                }
-            }
-            else if (isPointOnLine(cx2, cy2, linePoint1x, linePoint1y, linePoint2x, linePoint2y))
-            {
-                if (cy2 > cy1) //只保证上端点+1
-                {
-                    count++;
-                }
-            }
-            else if (isLineIntersect(cx1, cy1, cx2, cy2, linePoint1x, linePoint1y, linePoint2x, linePoint2y)) //已经排除平行的情况
-            {
-                count++;
-            }
-        }
-
-        if (count % 2 == 1)
-        {
-            isInside = true;
-        }
-
-        return isInside;
-    }
-
-    /**
-     * 判断线段是否相交
-     * 如果线段交于一点
-     * 不算相交
-     */
-    bool isLineIntersect(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
-    {
-        /**
-         * 快速排斥：
-         * 两个线段为对角线组成的矩形
-         * 如果这两个矩形没有重叠的部分，那么两条线段是不可能出现重叠的
-        */
-        if (!(MIN(x1, x2) < MAX(x3, x4) && MIN(y3, y4) < MAX(y1, y2) && MIN(x3, x4) < MAX(x1, x2) && MIN(y1, y2) < MAX(y3, y4))) //这里的确如此，这一步是判定两矩形是否相交
-            /**
-             * 1.线段ab的低点低于cd的最高点（可能重合）
-             * 2.cd的最左端小于ab的最右端（可能重合）
-             * 3.cd的最低点低于ab的最高点（加上条件1，两线段在竖直方向上重合）
-             * 4.ab的最左端小于cd的最右端（加上条件2，两直线在水平方向上重合）
-             * 综上4个条件，两条线段组成的矩形是重合的
-             * 特别要注意一个矩形含于另一个矩形之内的情况
-             */
-            return false;
-        /**
-         * 跨立实验：
-         * 如果两条线段相交，那么必须跨立，就是以一条线段为标准，另一条线段的两端点一定在这条线段的两段
-         * 也就是说a b两点在线段cd的两端，c d两点在线段ab的两端
-        */
-        double u, v, w, z; //分别记录两个向量
-        u = (x3 - x1) * (y2 - y1) - (x2 - x1) * (y3 - y1);
-        v = (x4 - x1) * (y2 - y1) - (x2 - x1) * (y4 - y1);
-        w = (x1 - x3) * (y4 - y3) - (x4 - x3) * (y1 - y3);
-        z = (x2 - x3) * (y4 - y3) - (x4 - x3) * (y2 - y3);
-        return (u * v <= 0.00000001 && w * z <= 0.00000001);
-    }
-
-    /**
-     * 判断线段是否相交
-     */
-    bool isLineIntersect(const CPoint &p1, const CPoint &p2, const CPoint &p3, const CPoint &p4)
-    {
-        return isLineIntersect(p1._x, p1._y,
-                               p2._x, p2._y,
-                               p3._x, p3._y,
-                               p4._x, p4._y);
-    }
-
-    /**
-     * 判断线段是否相交
-     */
-    bool isLineIntersect(const CLine &line_1, const CLine &line_2)
-    {
-        return isLineIntersect(line_1._p1._x, line_1._p1._y,
-                               line_1._p2._x, line_1._p2._y,
-                               line_2._p1._x, line_2._p1._y,
-                               line_2._p2._x, line_2._p2._y);
-    }
-
-    /**
-     * 判断多边形是否相交
-     */
-    bool isPolygonsIntersect(const CPolygon &p1, const CPolygon &p2)
-    {
-        int i, j;
-        // 1. point judge
-        for (i = 0; i < p1._vecPoints.size(); i++)
-        {
-            if (isPointInPolygon(p1._vecPoints[i]._x, p1._vecPoints[i]._y, p2))
-                return true;
-        }
-
-        for (i = 0; i < p2._vecPoints.size(); i++)
-        {
-            if (isPointInPolygon(p2._vecPoints[i]._x, p2._vecPoints[i]._y, p1))
-                return true;
-        }
-
-        // 2. line judge
-        for (i = 0; i < p1._vecPoints.size(); i++)
-        {
-            for (j = 0; j < p2._vecPoints.size(); j++)
-            {
-                if (isLineIntersect(p1._vecPoints[i], p1._vecPoints[(i + 1) % p1._vecPoints.size()],
-                                    p2._vecPoints[j], p2._vecPoints[(j + 1) % p2._vecPoints.size()]))
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-};
-
-class ISpTriangleInPolygon : public ISpPolygonAlgorithm
-{
-public:
-    ISpTriangleInPolygon() {}
-    ~ISpTriangleInPolygon() {}
-
-    /**
-     * 判断三角形是否与多边形相交
-     */
-    bool isTriangleInPolygon(const CPolygon &t, const CPolygon &p)
-    {
-        int i, j;
-
-        // 不是三角形
-        if (t._vecPoints.size() > 3)
-            return false;
-
-        // 判断三角形是否和多边形有共同点
-        int cout = 0;
-        for (i = 0; i < t._vecPoints.size(); i++)
-        {
-            for (j = 0; j < p._vecPoints.size(); j++)
-            {
-                if (t._vecPoints[i]._x == p._vecPoints[j]._x && t._vecPoints[i]._y == p._vecPoints[j]._y)
-                    cout++;
-            }
-        }
-        // 如果三个顶点都在多边形上 必然相交
-        if (cout == 3)
-        {
-            return true;
-        }
-        // 三角形和多边形共边
-        // 判断点是否在多边形内
-        if (cout == 2)
-        {
-            for (i = 0; i < t._vecPoints.size(); i++)
-            {
-                // 点在多边形里面 相交
-                if (isPointInPolygon(t._vecPoints[i]._x, t._vecPoints[i]._y, p))
-                    return true;
-            }
-            // 点不在多边形内 判断动点线段是否相交
-            for (i = 0; i < p._vecPoints.size(); i++)
-            {
-                if (isLineIntersect(t._vecPoints[0], t._vecPoints[1],
-                                    p._vecPoints[i], p._vecPoints[(i + 1) % p._vecPoints.size()]))
-                    return true;
-            }
-            // 另外一条边
-            for (i = 0; i < p._vecPoints.size(); i++)
-            {
-                if (isLineIntersect(t._vecPoints[0], t._vecPoints[2],
-                                    p._vecPoints[i], p._vecPoints[(i + 1) % p._vecPoints.size()]))
-                    return true;
-            }
-
-            // 如果三角形共边 但是点不在多边形内 且动点线段和多边形没有相交
-            // 则三角形和多边形不相交
-            return false;
-        }
-
-        // 正常判断
-        return isPolygonsIntersect(t, p);
-    }
 };
 
 #endif
